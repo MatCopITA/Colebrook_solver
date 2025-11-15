deefaf6bbcfb2fe53b2b2faad87f946ba34bce1f
MatCopITA <m.copett05@gmail.com>
2025-11-15 10:35:58 +0100

    commit

--- Colebrook_solver/Program.cs
+++ Colebrook_solver/Program.cs
@@ -1,22 +1,36 @@
 ﻿using System;
-using System.Threading;
 
 class Program
 {
   static void Main(string[] args)
   {
-    Console.WriteLine("Enter the fluid velocity (m/s): ");
-    string v_str = Console.ReadLine(); // Velocity in m/s
-    Console.WriteLine("Enter the pipe diameter (m): ");
-    string D_str = Console.ReadLine(); // Diameter in meters
-    Console.WriteLine("Enter the pipe roughness (μm): ");
-    string K_str = Console.ReadLine(); // Roughness in meters
-    Console.WriteLine("Enter the fluid density (kg/m^3): ");
-    string rho_str = Console.ReadLine(); // Density in kg/m^3
-    Console.WriteLine("Enter the fluid dynamic viscosity (Pa.s): ");
-    string mu_str = Console.ReadLine(); // Dynamic viscosity in Pa.s
+    Console.Clear();
 
-    string Q_str = null; // Volumetric flow rate in m^3/s
+    string title = "\x1b[1m- Solver for Reynolds Number (Re) and Friction Factor (f(Re, K)) -\x1b[0m";
+    Console.WriteLine($"{Environment.NewLine}");
+    Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (title.Length / 2)) + "}", title));
+
+    Console.WriteLine();
+    Console.WriteLine("Enter the fluid velocity (m/s): ");
+    string? v_str = Console.ReadLine(); // Velocity in m/s
+
+    Console.WriteLine("Enter the pipe diameter (m): ");
+    string? D_str = Console.ReadLine(); // Diameter in meters
+
+    Console.WriteLine("Enter the pipe roughness (μm): (0 if smooth)");
+    string? K_str = Console.ReadLine(); // Roughness in meters 
+
+    Console.WriteLine("Enter the fluid density (kg/m^3): ");
+    string? rho_str = Console.ReadLine(); // Density in kg/m^3
+
+    Console.WriteLine("Enter the fluid dynamic viscosity (Pa*s): ");
+    string? mu_str = Console.ReadLine(); // Dynamic viscosity in Pa.s
+
+    if (v_str == null || D_str == null || K_str == null || rho_str == null || mu_str == null)
+    {
+      Console.WriteLine("Invalid input. Please provide all required values.");
+      return;
+    }
 
     double v = Convert.ToDouble(v_str);
     double D = Convert.ToDouble(D_str);
@@ -26,39 +40,53 @@ class Program
 
     double Re = (rho * v * D) / mu; // Reynolds number
 
+    Console.WriteLine("Reynolds number (Re): " + Re);
+
     if (K == 0)
     {
-      if (Re < 2000)
+      if (Re < 2100)
       {
         Console.WriteLine("f(Re) (Laminar, Smooth Pipe): " + (16 / Re));
       }
       else
       {
-        Console.WriteLine("f(Re) (Turbulent, Smooth Pipe): " + (0.079 / Math.Pow(Re, -0.25)));
+        Console.WriteLine("f(Re) (Turbulent, Smooth Pipe): " + (0.079 * Math.Pow(Re, -0.25))); //Blasius formula
       }
     }
     else
     {
-      double FindColebrook()
+      if (Re < 2100)
       {
-        double f_local = 0.02;
-        double fOld;
-        int maxIterations = 10000;
-        double tolerance = 1e-10;
-
-        for (int i = 0; i < maxIterations; i++)
-        {
-          fOld = f_local;
-
-          f_local = 1.0 / Math.Pow(-1.7 * Math.Log(((K*Math.Pow(10,-6)) / D) + (4.67 / (Re * Math.Sqrt(f_local))) + 2.28), 2);
-          if (f_local <= 0) throw new InvalidOperationException("Non-physical friction factor computed.");
-
-          if (Math.Abs(f_local - fOld) < tolerance) break;
-        }
-        return f_local;
+        Console.WriteLine("f(Re, K) (Laminar, Rough Pipe): " + (16 / Re));
       }
-      double f = FindColebrook();
-      Console.WriteLine("f(Re, K) (Turbulent, Rough Pipe): " + f);
+      else
+      {
+        double FindColebrook()
+        {
+          double f_local = 1e-10;
+          double f_old;
+          double maxIter = 1e10;
+          double tolerance = 1e-10;
+
+          for (int i = 0; i < maxIter; i++)
+          {
+            f_old = f_local;
+
+            f_local = 1.0 / Math.Pow(-1.7 * Math.Log(((K * Math.Pow(10, -6)) / D) + (4.67 / (Re * Math.Sqrt(f_local))) + 2.28), 2); //Colebrook formula
+
+            if (f_local <= 0) throw new InvalidOperationException("Non-physical friction factor computed.");
+
+            if (Math.Abs(f_local - f_old) < tolerance) break;
+          }
+          return f_local;
+        }
+
+        double f = FindColebrook();
+        Console.WriteLine("f(Re, K) (Turbulent, Rough Pipe): " + f);
+      }
     }
+    Console.WriteLine($"{Environment.NewLine}Press any key to reset");
+    Console.ReadKey();
+    Main(args);
   }
 }
--- Colebrook_solver/obj/Release/net10.0/Colebrook_solver.AssemblyInfo.cs
+++ Colebrook_solver/obj/Release/net10.0/Colebrook_solver.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Colebrook_solver")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b9fae3d47da5106405f85b0620fc9dad6b496d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Colebrook_solver")]
